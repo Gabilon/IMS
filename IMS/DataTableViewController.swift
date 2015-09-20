3becf32b98717b6ac6d7df61ba4e529fe78b868c
Gabriel Andres <gabrielandres2011@hotmail.com>
2015-09-19 22:36:00 -0400

    Few upcoming changes
    Deleted Parse fetch and added TableView’s..
    
    Future: Tapp a Cell -> expand -> FetchParseData

---
+++ IMS/DataTableViewController.swift
@@ -0,0 +1,54 @@
+//
+//  DataTableViewController.swift
+//  IMS
+//
+//  Created by Gabriel Bermudez on 9/19/15.
+//  Copyright (c) 2015 Gabriel Bermudez. All rights reserved.
+//
+
+import UIKit
+
+class DataTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        //Return the number of sections
+        return 3
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //Return the number of rows in the section
+        return 5
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("LabelCell", forIndexPath: indexPath) as! UITableViewCell
+        
+        cell.textLabel?.text = "Section \(indexPath.section) Row \(indexPath.row)"
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Section \(section)"
+    }
+    
+    
+
+}
